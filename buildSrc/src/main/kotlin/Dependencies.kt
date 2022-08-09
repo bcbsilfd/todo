f232f0df7b4d1b003ddb49cfba2537117d87381e
bcbsilfd <workhome.pro@gmail.com>
2022-08-09 09:46:06 +0300

    Add buildSrc kotlin-dsl

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,9 @@
+object Dependencies {
+    val viewModel by lazy { "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1" }
+    val material by lazy { "com.google.android.material:material:1.6.1" }
+    val junit by lazy { "junit:junit:4.13.2" }
+    val androidXcore by lazy { "androidx.core:core-ktx:1.8.0" }
+    val appCompat by lazy { "androidx.appcompat:appcompat:1.4.2" }
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:2.1.4" }
+    val lifecycleRuntime by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1" }
+}
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -32,12 +32,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation(Dependencies.androidXcore)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.constraintLayout)
+    implementation(Dependencies.lifecycleRuntime)
+    implementation(Dependencies.viewModel)
+    implementation(Dependencies.material)
 
-    testImplementation("junit:junit:4.13.2")
+    testImplementation(Dependencies.junit)
 }
